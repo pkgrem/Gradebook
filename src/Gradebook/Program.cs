85a023f350733f373124734afcb7e8239d8d7c02
Paddy <paddy.goode2@mail.dcu.ie>
2020-05-19 13:12:44 +0100

    Add files via upload
    Pluralsight C# Fundamentals

---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello " + args[0] + "!");
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else 
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
